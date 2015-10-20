a3e1949076dcc680f048e4e4d5479551c1165060
Raiki Kawada <rarudo.act@gmail.com>
2015-10-20 20:07:04 +0900

    修正基礎課題

---
+++ src/KisoKadai1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class KisoKadai1 {
+	public static void main(String[] args)throws IOException {
+		int x;
+    	int y;
+    	System.out.print("x‚Æy‚Ì“ü—Í:");
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	 try{
+    	x = Integer.parseInt(br.readLine());
+        y = Integer.parseInt(br.readLine());
+       
+    	for(int i=x;i>=1;i--){
+    		for(int j=1;j<=y;j++){
+    			  System.out.print(i*j+",");
+    			  }
+    		System.out.println("");
+    	}
+        }
+        catch(NumberFormatException e){
+	    	  System.err.println("ƒGƒ‰[");
+	    	  }
+        }
+    	}
+	
+	
--- src/KisoKadai3.java
+++ src/KisoKadai3.java
@@ -1,33 +1,67 @@
 import java.io.*;
+
 public class KisoKadai3 {
 	public static void main(String[] args)throws IOException {
 		String a = "";
+		String d = "";
+		System.out.println("dir");
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	System.out.println("ファイルの検索をします");
+    	//System.out.println("ファイルの検索をします");
+		//d =br.readLine();
+    	
 		a =br.readLine();
-    	File file = new File("C:\\Users\\internous\\"+ a +".txt");
-    	try{
-    		System.out.print("ファイルの検索中:"+a);
-    		
+		
+		System.out.println("file");
+    	br = new BufferedReader(new InputStreamReader(System.in));
+    	//System.out.println("ファイルの検索をします");
+    	
+		d =br.readLine();
+		
+		//a =br.readLine();
+    	File file = new File("C:\\Users\\internous\\"+a);
+    	//try{
+    		System.out.println("フォルダ検索"+a);
+    		if(file.exists()){
+    			System.out.println(a);
+    		}else if(file.mkdir()){
+             System.out.println("フォルダを作成しました。");
+    		}
+    	//}
+    	//catch(Exception e){
+  	     // System.out.println(e);
+  	     // }
+    	//try{
+    		System.out.print("ファイルの検索中:"+d);
+    		file = new File("C:\\Users\\internous\\"+a+"\\"+d+".txt");
     		if(file.exists()){
     			System.out.println("ファイルを追記しますか？：1");
     			System.out.println("それともファイルの読み込みをしますか？:2");
     			int b =0;
     			while(b < 1 || 2 < b){
     			FileWriter fw=new FileWriter(file, true);
+    			
+    			
     			String str=br.readLine();
+    			
     			b = Integer.parseInt(str);
+    			
     			if(b == 1){
     				System.out.print("ファイルの追記をします。");
+    				
+    				
     				String c = br.readLine();
     					fw.write("\r\n"+c);
     					fw.close();
-    					System.out.println(c+"を上書きしました。");
+    					System.out.println("追記しました。");
     					}else if(b == 2){
+    						FileReader fr = new FileReader("C:\\Users\\internous\\"+a+"\\"+d);
+    						int ch;
     						System.out.println("ファイルを読み込みます。");
-    						//テキスト出力
-    						String str1 = br.readLine();
-    						System.out.println(str1);
+    						while ((ch = fr.read()) != -1) {
+    						System.out.print((char) ch);
+    					    }
+    						System.out.print("\n終了します");
+    			    		System.exit(0);
     						}else if(b < 1 || 2 < b){
 							System.out.println("1か2で答えてください。");
 						}
@@ -43,18 +77,18 @@ public class KisoKadai3 {
     	    	//書き込む文字
     	    	fw.write(str);
     	    	fw.close();
-    	    	System.out.println(str+"を保存しました。");
+    	    	System.out.println("保存しました。");
     	    	}
     	    			
     		System.out.print("終了します");
     		System.exit(0);
-    		}
+    	//	}
     	//エラー
-    	catch(FileNotFoundException e){
-    	      System.out.println(e);
-    	      }
-    	catch(IOException e){
-    		System.out.println(e);
-    		}
+    	//catch(FileNotFoundException e){
+    	      //System.out.println(e);
+    	      //}
+    	//catch(IOException e){
+    		//System.out.println(e);
+    		//}
     	}
-	}
+}
